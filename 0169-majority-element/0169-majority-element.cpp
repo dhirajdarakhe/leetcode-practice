d18c73e799c80fc69e17bd8f19d8c7183fb138d9
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2023-02-03 18:12:47 +0530

    Time: 18 ms (85.06%), Space: 19.6 MB (72.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int mx = INT_MIN;
-        int ans = 0;
+        // o(nlogn)
+//         sort(nums.begin(), nums.end());
+//         int mx = INT_MIN;
+//         int ans = 0;
+//         int ct = 0;
+//         int i = 0;
+//        for( i=1;i<nums.size();++i)
+//        {
+//            if(nums[i-1] == nums[i]) ct++;
+//            else{
+//                if(ct >= nums.size()/2)
+//                {
+//                    ans = nums[i-1];
+                   
+//                }
+//                ct =0;
+//            }
+//        }
+//               if(ct >= nums.size()/2)
+//                {
+//                    ans = nums[i-1];
+                   
+//                }
+//         return ans;
+         // o(n)
         int ct = 0;
-        int i = 0;
-       for( i=1;i<nums.size();++i)
-       {
-           if(nums[i-1] == nums[i]) ct++;
-           else{
-               if(ct >= nums.size()/2)
-               {
-                   ans = nums[i-1];
-                   
-               }
-               ct =0;
-           }
-       }
-              if(ct >= nums.size()/2)
-               {
-                   ans = nums[i-1];
-                   
-               }
-        return ans;
+        int element = 0;
+        for(auto x : nums)
+        {
+            if(ct) 
+            {
+               if(element==x) ct++;
+                else ct--;
+            }
+            else 
+            {
+                ct++;
+                element=x;
+            }
+        }
+        return element;
         
     }
 };
