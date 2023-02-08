28608df82cb78b0baab751a0055fdfa2fa20d656
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2023-02-08 17:46:26 +0530

    Time: 19 ms (40.31%), Space: 15.8 MB (84.39%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,10 +1,42 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        map<int, int> m;
+        int ct1= 0;
+        int ct2 = 0;
+        int n1 = -1;
+        int n2 = -1;
+        for(auto x : nums)
+        {   
+            if(n1 == x) ct1++;
+            else if(n2 == x) ct2++;
+            else if(ct1 == 0) {n1 = x; ct1++;}
+            else if(ct2 == 0) { n2 = x; ct2++;}
+            else {
+                ct1--;
+                ct2--;
+            }
+        }
+        ct1=0;ct2=0;
+        for(auto x : nums)
+        {
+            if(x == n1) ct1++;
+            if(x == n2) ct2++;
+        }
         vector<int> ans;
-        for(auto x : nums) m[x]++;
-        for(auto x : m) if(x.second > nums.size()/3) ans.push_back(x.first);
+        cout<<ct1<<" "<<ct2<<endl;
+        if(ct1>nums.size()/3 ) ans.push_back(n1);
+        if(ct2>nums.size()/3 && n1 != n2) ans.push_back(n2);
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         map<int, int> m;
+//         vector<int> ans;
+//         for(auto x : nums) m[x]++;
+//         for(auto x : m) if(x.second > nums.size()/3) ans.push_back(x.first);
+//         return ans;
+//     }
+// };
