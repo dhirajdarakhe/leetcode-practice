baaafaa1c57cd8176d3bdd57fe39f7215fc14760
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2023-02-25 23:18:47 +0530

    Time: 13 ms (73.73%), Space: 8.6 MB (77.44%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -2,35 +2,47 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
     
-        // map< char , int > mp
+        // // map< char , int > mp;
         // sort(p.begin(), p.end());
         // vector<int> ans;
         // for(int i =0 ; i + p.size() <= s.size(); ++i)
         // {     
         //        string k = s.substr(i, p.size());
         //        sort(k.begin(), k.end());
-        //        // cout<<i<< " "<< k<<endl;
         //        if( p == k) ans.push_back(i);
         // }
         // return ans;
         
         
-        map< char , int > mp, kp;
+        // map< char , int > mp, kp;
+        // vector<int> ans;
+        // for(int i = 0; i < 26; ++i) mp[i + 'a'] = 0;
+        // for(int i = 0; i < 26; ++i) kp[i + 'a'] = 0;
+        // for(auto x : p) mp[x]++;
+        // for(int i = 0; i < s.size(); ++i)
+        // {
+        //     kp[s[i]]++;
+        //     if(i >= p.size())
+        //     {
+        //        kp[s[i-p.size()]]--;
+        //        // if(kp[s[i-p.size()]]==0) kp.erase(s[i-p.size()]);
+        //     }
+        //     if(kp==mp) ans.push_back(i-p.size()+1);
+        // }
+        // return ans;
+        
+        vector<int> mp(26), kp(26);
         vector<int> ans;
-        for(int i = 0; i < 26; ++i) mp[i + 'a'] = 0;
-        for(int i = 0; i < 26; ++i) kp[i + 'a'] = 0;
-        for(auto x : p) mp[x]++;
+        for(int i = 0; i < 26; ++i) mp[i] = 0;
+        for(int i = 0; i < 26; ++i) kp[i] = 0;
+        for(auto x : p) mp[x - 'a']++;
         for(int i = 0; i < s.size(); ++i)
         {
-            kp[s[i]]++;
-            if(i >= p.size())
-            {
-               kp[s[i-p.size()]]--;
-               // if(kp[s[i-p.size()]]==0) kp.erase(s[i-p.size()]);
-            }
-            if(kp==mp) ans.push_back(i-p.size()+1);
+            kp[s[i] - 'a']++;
+            if(i >= p.size())  kp[s[i-p.size()] - 'a']--;
+            if( kp == mp ) ans.push_back(i-p.size()+1);
         }
+       
         return ans;
-        
     }
 };
