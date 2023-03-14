5c8adcf6461ae28c1741a154417060cefc10e874
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2023-03-14 19:28:05 +0530

    Time: 303 ms (66.80%), Space: 134.5 MB (93.40%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> ans;
         for(int i = 0; i < nums.size(); ++i)
         {   
-            while(!dq.empty() && dq.front() <= i - k) dq.pop_front();
+            if(!dq.empty() && dq.front() <= i - k) dq.pop_front();
             while(!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
             dq.push_back(i);
             if(i >= k - 1 && dq.size()) ans.push_back(nums[dq.front()]);
